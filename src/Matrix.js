a64d0e4b782f3a534f3b35001d5026979546cdb6
Hack1434 <uckbx001@gmail.com>
2024-01-01 18:00:13 +0530

    commit

---
+++ src/Matrix.js
@@ -0,0 +1,33 @@
+import React from 'react';
+const Matrix = () =>{
+    return (
+        <div className="matrix">
+
+        <div className="row">
+
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+
+        </div>
+        <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+
+        </div>
+        <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+
+        </div>
+        
+        </div>
+    );
+};
+
+export default Matrix;
